e3caa480ba0cc1b93f307ce5e1f9f8fbddb1c6a0
EDUARD SHAVYRKO <edshav@protonmail.com>
2021-05-02 20:51:31 +0300

    implement multi-tier navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 export default () => {
   const ref = React.useRef(null);
+  const history = useHistory();
 
   React.useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        if (history.location.pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
