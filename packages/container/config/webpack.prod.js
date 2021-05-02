ec8fde4f41d004217e66b6d59743b21eec419c96
EDUARD SHAVYRKO <edshav@protonmail.com>
2021-05-02 13:04:26 +0300

    add marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
@@ -16,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
